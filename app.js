aad62a54274c02756e16fd62c399c0bd8ec58624
awwsman <usmanmvp@gmail.com>
2024-03-09 00:01:26 +0100

    final

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const nav = document.querySelector("nav");
 const openn = document.querySelector(".hamburger");
 const closee = document.querySelector(".icon-close");
-
+const date = document.querySelector(".date");
+console.log(new Date().getFullYear());
+date.textContent = new Date().getFullYear();
 openn.addEventListener("click", () => {
   nav.classList.add("active");
 });
